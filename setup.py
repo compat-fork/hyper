a71ea66b7aff6567f2252cb948b42a4e50895c98
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-08-21 16:12:54 -0700

    Attempt to set up CI

--- setup.py
+++ setup.py
@@ -69,15 +69,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
     install_requires=[
-        'h2>=2.4,<3.0,!=2.5.0', 'hyperframe>=3.2,<4.0', 'rfc3986>=1.1.0,<2.0', 'brotlipy>=0.7.0,<1.0'
+        'h2>=2.4', 'hyperframe>=3.2', 'rfc3986>=1.1.0', 'brotlipy>=0.7.0'
     ],
     tests_require=['pytest', 'requests', 'mock'],
     cmdclass={'test': PyTest},
@@ -88,15 +84,5 @@ setup(
     },
     extras_require={
         'fast': ['pycohttpparser'],
-        # Fallback to good SSL on bad Python versions.
-        ':python_full_version < "2.7.9"': [
-            'pyOpenSSL>=0.15', 'service_identity>=14.0.0'
-        ],
-        # PyPy with bad SSL modules will likely also need the cryptography
-        # module at lower than 1.0, because it doesn't support CFFI v1.0 yet.
-        ':platform_python_implementation == "PyPy" and python_full_version < "2.7.9"': [
-            'cryptography<1.0'
-        ],
-        ':python_version == "2.7" or python_version == "3.3"': ['enum34>=1.0.4, <2']
     }
 )
